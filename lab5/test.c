13fccb0f6629bb9b75d6fe55aad7169650817968
Chuong Pham <phchuong1998@gmail.com>
2024-03-22 15:03:59 -0400

    ADded Delete Node

--- lab5/test.c
+++ lab5/test.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Node declaration for the BST
 typedef struct Node_struct {
 	int key;
 	struct Node_struct *left;
 	struct Node_struct *right;
 } Node;
 
-// A utility function to create a new BST node
+// Create a new Node from the struct Node
 Node* createNode(int item)
 {
 	Node* newNode = (Node*) malloc (sizeof(Node));
@@ -16,7 +17,7 @@ Node* createNode(int item)
 	return newNode;
 }
 
-// A utility function to do inorder traversal of BST
+// Print the BST in Inorder traversal
 void inorder(Node* node)
 {
 	if (node != NULL) 
@@ -27,13 +28,13 @@ void inorder(Node* node)
 	}
 }
 
-// A utility function to check if the node is an External node
+// Check if the node is an External node
 int isExternal(Node* node)
 {
 	return node == NULL;
 }
 
-// Utility function to search a key in a BST
+// Function to Search a key in a BST
 Node* TreeSearch(Node* node, int key)
 {
 	// Base Cases: node is null or key is present at node
@@ -53,7 +54,7 @@ Node* TreeSearch(Node* node, int key)
 	}
 }
 
-// A utility function to insert a new node with given key in BST
+// Function to insert a new node with a value (key)
 Node* Insert(Node* node, int key)
 {
 	// Base case, Node is empty so create a new node with NULL left and right child
@@ -62,18 +63,18 @@ Node* Insert(Node* node, int key)
 		return createNode(key);
 	}
 
-	// Otherwise, recur down the tree
-	if (key <= node->key) // If Key is less than node, move to the left Child
+	// Otherwise, do recursive call down the tree to determine which Left or Right child to Insert
+	if (key <= node->key) // If Key is less than Node, move to the left Child
 	{
 		node->left = Insert(node->left, key);
 	}
-	else if (key > node->key) // // If Key is more than node, move to the right Child. Add '>=' if u want equal key to move on the right tree instead of left tree
+	else if (key > node->key) // If Key is larger than Node, move to the right Child. Add '>=' if u want equal key to move on the right tree instead of left tree
 	{
 		node->right = Insert(node->right, key);
 	}
 }
 
-// // Insertion Method based on the slide, doesnt work, waste 1.5 hours tryinh to figure out why, the TreeSearch doesnt move the fucking Node
+// // Insertion Method based on the slide, doesnt work, waste 1.5 hours trying to figure out why, the TreeSearch() doesnt move the fucking Node!
 // Node* Insert(Node* node, int key)
 // {
 // 	Node* keyNode = TreeSearch(node, key);
@@ -88,92 +89,96 @@ Node* Insert(Node* node, int key)
 // 	}
 // }
 
-// /* Given a binary search tree and a key, this function deletes the key and returns the new node */
-// struct Node* deleteNode(struct Node* node, int k)
-// {
-// 	// Base case
-// 	if (node == NULL)
-// 		return node;
+/* Given a binary search tree and a key, this function deletes the key and returns the new node */
+Node* deleteNode(Node* node, int key)
+{
+	// Base case
+	if (isExternal(node))
+	{
+		return node;
+	}
 
-// 	// Recursive calls for ancestors of
-// 	// node to be deleted
-// 	if (node->key > k) {
-// 		node->left = deleteNode(node->left, k);
-// 		return node;
-// 	}
-// 	else if (node->key < k) {
-// 		node->right = deleteNode(node->right, k);
-// 		return node;
-// 	}
+	// Otherwise, do recursive call down the tree of the node to be deleted
+	if (key < node->key) 
+	{
+		node->left = deleteNode(node->left, key);
+		return node;
+	}
+	else if (key > node->key) 
+	{
+		node->right = deleteNode(node->right, key);
+		return node;
+	}
 
-// 	// We reach here when node is the node
-// 	// to be deleted.
+	// We reach here when this is the node to be deleted.
 
-// 	// If one of the children is empty
-// 	if (node->left == NULL) {
-// 		struct Node* temp = node->right;
-// 		free(node);
-// 		return temp;
-// 	}
-// 	else if (node->right == NULL) {
-// 		struct Node* temp = node->left;
-// 		free(node);
-// 		return temp;
-// 	}
+	// Case 1: If one of the children is empty
+	if (node->left == NULL) 
+	{
+		Node* temp = node->right;
+		free(node);
+		return temp;
+	}
+	else if (node->right == NULL) 
+	{
+		Node* temp = node->left;
+		free(node);
+		return temp;
+	}
 
-// 	// If both children exist
-// 	else {
+	// Case 2: If both children exist
+	else 
+	{
+		Node* succParent = node;
 
-// 		struct Node* succParent = node;
+		// Find successor
+		Node* succ = node->right;
+		while (succ->left != NULL) {
+			succParent = succ;
+			succ = succ->left;
+		}
 
-// 		// Find successor
-// 		struct Node* succ = node->right;
-// 		while (succ->left != NULL) {
-// 			succParent = succ;
-// 			succ = succ->left;
-// 		}
+		// Delete successor. Since successor
+		// is always left child of its parent
+		// we can safely make successor's right
+		// right child as left of its parent.
+		// If there is no succ, then assign
+		// succ->right to succParent->right
+		if (succParent != node)
+			succParent->left = succ->right;
+		else
+			succParent->right = succ->right;
 
-// 		// Delete successor. Since successor
-// 		// is always left child of its parent
-// 		// we can safely make successor's right
-// 		// right child as left of its parent.
-// 		// If there is no succ, then assign
-// 		// succ->right to succParent->right
-// 		if (succParent != node)
-// 			succParent->left = succ->right;
-// 		else
-// 			succParent->right = succ->right;
+		// Copy Successor Data to node
+		node->key = succ->key;
 
-// 		// Copy Successor Data to node
-// 		node->key = succ->key;
-
-// 		// Delete Successor and return node
-// 		free(succ);
-// 		return node;
-// 	}
-// }
+		// Delete Successor and return node
+		free(succ);
+		return node;
+	}
+}
 
 // Driver Code
 int main()
 {
-	/* Let us create following BST
-			50
-		/	 \
-		30	 70
-		/ \ / \
-	20 40 60 80 */
+    /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
 	Node* root = NULL;
 	root = Insert(root, 50);
 	Insert(root, 30);
 	Insert(root, 20);
 	Insert(root, 40);
-	Insert(root, 40);
+	// Insert(root, 40);
 	Insert(root, 70);
 	Insert(root, 60);
-	Insert(root, 60);
+	// Insert(root, 60);
 	Insert(root, 80);
-	Insert(root, 45);
-	Insert(root, 67);
+	// Insert(root, 45);
+	// Insert(root, 67);
 
 	// Print inorder traversal of the BST
 	inorder(root);
@@ -195,23 +200,23 @@ int main()
 	else
 		printf("%d found\n", key);
 
-    // // Delete
-	// printf("Original BST: ");
-	// inorder(root);
+    // Delete
+	printf("Original BST: ");
+	inorder(root);
 
-	// printf("\n\nDelete a Leaf Node: 20\n");
-	// root = deleteNode(root, 20);
-	// printf("Modified BST tree after deleting Leaf Node:\n");
-	// inorder(root);
+	printf("\n\nDelete a Leaf Node: 20\n");
+	root = deleteNode(root, 20);
+	printf("Modified BST tree after deleting Leaf Node:\n");
+	inorder(root);
 
-	// printf("\n\nDelete Node with single child: 70\n");
-	// root = deleteNode(root, 70);
-	// printf("Modified BST tree after deleting single child Node:\n");
-	// inorder(root);
+	printf("\n\nDelete Node with single child: 70\n");
+	root = deleteNode(root, 70);
+	printf("Modified BST tree after deleting single child Node:\n");
+	inorder(root);
 
-	// printf("\n\nDelete Node with both child: 50\n");
-	// root = deleteNode(root, 50);
-	// printf("Modified BST tree after deleting both child Node:\n");
-	// inorder(root);    
+	printf("\n\nDelete Node with both child: 50\n");
+	root = deleteNode(root, 50);
+	printf("Modified BST tree after deleting both child Node:\n");
+	inorder(root);    
 	return 0;
 }
